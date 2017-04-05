b1f040335ec2ef94a0634db298a9ba6cfd2bfa54
Simo Suominen <simppas@gmail.com>
2017-04-05 22:03:47 +0300

    Improve settings module

--- settings/manifests/init.pp
+++ settings/manifests/init.pp
@@ -1,7 +1,14 @@
 class settings {
 
+	file {'/home/suomisim/.config/xfce4/panel/whiskermenu-1.rc':
+		content => template('settings/whiskermenu-1.rc.erb')
 
+	}
 
+	file {'/home/suomisim/.config/xfce4/terminal/terminalrc':
+		content => template('settings/terminalrc.erb')
+
+	}
 
 
 }
