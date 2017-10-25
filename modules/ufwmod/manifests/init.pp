e813f50ea1ae4e0f0f0c321f13e72d09efd94118
Simo Suominen <simppas@gmail.com>
2017-10-25 11:23:22 +0300

    Update ufwmod module

--- modules/ufwmod/manifests/init.pp
+++ modules/ufwmod/manifests/init.pp
@@ -1,26 +1,31 @@
 class ufwmod {
-
+	
+	# Komentojen polun määrittely
 	Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ] }
-
+	
+	# Paketin asennus
 	package { 'ufw':
 		ensure => 'installed',
-		allowcdrom => 'true',
 	}
-
+	
+	# Komennon ajo
 	exec { 'ufw-enable':
 		command => 'ufw enable',
 		require => Package['ufw'],
 	}
+	# Asetustiedoston muokkaus (IPv4)
 	file {'/etc/ufw/user.rules':
 		content => template('ufwmod/user.rules.erb'),
 		require => Package['ufw'],
 		notify => Service['ufw'],
 	}
+	# Asetustiedoston muokkaus (IPv6)
 	file {'/etc/ufw/user6.rules':
 		content => template('ufwmod/user6.rules.erb'),
 		require => Package['ufw'],
 		notify => Service['ufw'],
 	}
+	# Servicen toiminnan varmistaminen
 	service { 'ufw':
 		ensure  => 'true',
 		enable  => 'true',
