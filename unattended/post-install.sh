27cae8a92a2b9597a97f61af12ed59f3d8e60a7c
Simo Suominen <a1501104@myy.haaga-helia.fi>
2017-11-10 15:11:37 +0200

    Update post-install.sh

--- unattended/post-install.sh
+++ unattended/post-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-git clone https://github.com/suomisim/puppet
+git clone https://github.com/suomisim/puppet /home/suomisim/puppet
 
 puppet apply --modulepath=/home/suomisim/puppet/modules -e "include setupmodv1"
