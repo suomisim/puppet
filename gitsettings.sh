b02167189efe238eeed6dc5fe4f0ece5afeeeacf
Simo Suominen <simppas@gmail.com>
2017-11-21 09:59:59 +0000

    Update git settings script

---
+++ gitsettings.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# A script to set up git stuff
+
+git config --global user.email "suomisim@outlook.com" && git config --global user.name "Simo Suominen"
+git config --global credential.helper "cache --timeout=3600"
