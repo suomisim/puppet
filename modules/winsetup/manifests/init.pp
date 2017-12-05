af5b73b4e20e2947e7428088376ea9e7d78bc486
Simo Suominen <suomisim@outlook.com>
2017-12-05 15:31:05 +0200

    Update winsetup

--- modules/winsetup/manifests/init.pp
+++ modules/winsetup/manifests/init.pp
@@ -11,6 +11,28 @@ class winsetup {
         data => '2',
         require => Registry_key['HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate'],
     }
+	# Format a raw drive to E: and name it "Steam"
+	exec {'pwsh-format':
+		command => '$(Get-Disk | Where partitionstyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -DriveLetter E -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "Steam" -Confirm:$false),
+		provider => powershell,
+		require => Package,
+	}
+	# Install and run steam to E:\Steam
+    file {'C:\Puppetfiles\Steam.zip':
+                source => 'puppet:///modules/winapps/Steam.zip',
+                source_permissions => ignore,
+				require => Exec['pwsh-format'],
+    }
+    file {'C:\Puppetfiles\extract-steam.bat':
+                source => 'puppet:///modules/winapps/extract-steam.bat',
+                source_permissions => ignore,
+                require => File['C:\Puppetfiles\Steam.zip'],
+    }
+    exec {'extract-steam':
+                command => 'extract-steam.bat',
+                require => File["C:\Puppetfiles\extract-steam.bat"],
+                path => 'C:\Puppetfiles',
+    }
 	# Create user
 	user {'suomisim':
 		name      => 'suomisim',
@@ -47,28 +69,6 @@ class winsetup {
 		type => dword,
 		data => '1',
 	}
-	# Format a raw drive to E: and name it "Steam"
-	exec {'pwsh-format':
-		command => '$(Get-Disk | Where partitionstyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -DriveLetter E -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "Steam" -Confirm:$false),
-		provider => powershell,
-		require => Package,
-	}
-	# Install and run steam to E:\Steam
-    file {'C:\Puppetfiles\Steam.zip':
-                source => 'puppet:///modules/winapps/Steam.zip',
-                source_permissions => ignore,
-				require => Exec['pwsh-format'],
-    }
-    file {'C:\Puppetfiles\extract-steam.bat':
-                source => 'puppet:///modules/winapps/extract-steam.bat',
-                source_permissions => ignore,
-                require => File['C:\Puppetfiles\Steam.zip'],
-    }
-    exec {'extract-steam':
-                command => 'extract-steam.bat',
-                require => File["C:\Puppetfiles\extract-steam.bat"],
-                path => 'C:\Puppetfiles',
-    }
 	windows::shortcut { 'C:\Users\suomisim\Desktop\Steam.lnk':
 		target      => 'E:\Steam\Steam.exe',
 		description => 'Steam',
