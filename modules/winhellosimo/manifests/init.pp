8855e0bb58b06d3c0210f1e2f11defbdec8527aa
Simo Suominen <suomisim@outlook.com>
2017-12-13 09:43:44 +0200

    Update winhellosimo

--- modules/winhellosimo/manifests/init.pp
+++ modules/winhellosimo/manifests/init.pp
@@ -3,5 +3,8 @@ class winhellosimo {
 	file { 'c:\hellosimo.txt':
 		content => "Hello Simo!\n",
 	}
+	file { 'C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\StartUp\\SetupCompleteStartUp.cmd':
+		ensure => absent,
+	}
 }
 
